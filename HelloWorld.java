134d8c15f2f79bcd6585ac14c6f91e3c4276ba5e
Unknown <s144146@student.dtu.dk>
2018-02-09 23:56:03 +0100

    beautify

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,18 +2,18 @@ public class HelloWorld {
 
 public static void main(String[] args) {
 
-String hello = "Hello World %nHello World %n";
+        String hello = "Hello World %nHello World %n";
 
-for (int i = 1; i<=10 ;i++ ) {
+        for (int i = 1; i<=10; i++ ) {
 
-  System.out.println("Hello World \nHello World");
+                System.out.println("Hello World \nHello World");
 
-  System.out.printf(hello);
+                System.out.printf(hello);
 
-  System.out.println(hello);
+                System.out.println(hello);
 
-    }
+        }
 
-  }
+}
 
 }
