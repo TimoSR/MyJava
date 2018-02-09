dc430dbc966312dc877a306742b1381a56a8d764
Unknown <s144146@student.dtu.dk>
2018-02-09 17:27:24 +0100

    HelloWorld java
    it works!

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+
+public static void main(String[] args) {
+
+String hello = "Hello World %nHello World %n";
+
+for (int i = 1; i<=10 ;i++ ) {
+
+  System.out.println("Hello World \nHello World");
+
+  System.out.printf(hello);
+
+  System.out.println(hello);
+
+    }
+
+  }
+
+}
