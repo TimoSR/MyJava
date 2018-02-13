5a219b00caefdb95fc96ebacaa7cdc55ff658e4a
Unknown <s144146@student.dtu.dk>
2018-02-13 12:10:15 +0100

    algo opstart

---
+++ Algoritmer/CutOff.java
@@ -0,0 +1,15 @@
+public class CutOff {
+
+public static void main(String[] args) {
+
+int[] numbers = new int[10];
+
+for (i = 0; i < numbers.length ; 1++ ) {
+
+  numbers[i] =+ i;
+
+  System.out.println(numbers[i]);
+  
+  }
+
+}
